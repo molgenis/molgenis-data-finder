21af99b02ccb8b112c8e06d0ccb5384ab0d0e9f9
davidruvolo51 <davidruvolo51@gmail.com>
2021-04-13 14:34:34 +0200

    added base structure for submenus

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,79 @@
     <div class="d-flex h-100">
       <aside class="menu h-100 p-3">
         <h2 class="h2">Find data</h2>
-        <p>Find data and customize the view.</p>
         <form @submit.prevent>
           <fieldset>
             <legend>Select a table</legend>
-            <p>Choose a table to start with.</p>
             <filter-menu @change="updateSelectedTable"/>
           </fieldset>
           <fieldset>
             <legend>Select a Freeze</legend>
             <filter-freeze v-model="selectedFreeze"></filter-freeze>
           </fieldset>
+          <div v-show="selectedTable === 'Samples'">
+            <fieldset>
+              <legend>Select Tissue Types</legend>
+              <!-- this should be a multifilter -->
+            </fieldset>
+            <fieldset>
+              <legend>Select Organisation</legend>
+              <!-- this should be a multifilter -->
+            </fieldset>
+            <fieldset>
+              <legend>Select an ERN</legend>
+              <!-- this should be a multifilter -->
+            </fieldset>
+          </div>
+          <div v-show="selectedTable === 'Patients'">
+            <fieldset>
+              <legend>Select Sex</legend>
+              <filter-sex v-model="selectedSex"></filter-sex>
+            </fieldset>
+            <fieldset>
+              <legend>Filter by Affected Status</legend>
+              <filter-affected-status v-model="selectedAffectedStatus"></filter-affected-status>
+            </fieldset>
+            <fieldset>
+              <legend>Filter by Solved Status</legend>
+            </fieldset>
+            <fieldset>
+              <legend>Filter by Phenotype</legend>
+            </fieldset>
+            <fieldset>
+              <legend>Select Organisation</legend>
+              <!-- this should be a multifilter -->
+            </fieldset>
+            <fieldset>
+              <legend>Select an ERN</legend>
+              <!-- this should be a multifilter -->
+            </fieldset>
+          </div>
+          <div v-show="selectedTable === 'Experiments'">
+            <fieldset>
+              <legend>Enrichment Kit</legend>
+            </fieldset>
+            <fieldset>
+              <legend>Library Source</legend>
+            </fieldset>
+            <fieldset>
+              <legend>Sequencing Type</legend>
+              <!-- this should be a multifilter -->
+            </fieldset>
+          </div>
+          <div v-show="selectedTable === 'Files'">
+            <fieldset>
+              <legend>File Type</legend>
+              <!-- this should be a multifilter -->
+            </fieldset>
+          </div>
         </form>
       </aside>
       <main class="p-3">
-        <p><strong>Selected Table:</strong> {{ selectedTable }}</p>
-        <p><strong>Selected Freezes:</strong> {{ selectedFreeze }}</p>
-<!-- {{ query() }}-->
+      <p><code>Table: {{ selectedTable }}</code></p>
+      <p><code>Freeze: {{ selectedFreeze }}</code></p>
+      <p><code>Sex: {{ selectedSex }}</code></p>
+      <p><code>Affected Status: {{ selectedAffectedStatus }}</code></p>
+      <!-- {{ query() }}-->
       </main>
     </div>
   </page-component>
@@ -29,16 +85,20 @@
 import PageComponent from '@molgenis/molgenis-ui-context/src/components/PageComponent.vue'
 import FilterMenu from './components/FilterMenu'
 import FilterFreeze from './components/FilterFreeze'
+import FilterSex from './components/FilterSex'
+import FilterAffectedStatus from './components/FilterAffectedStatus'
 // import axios from 'axios'
 
 export default {
   data () {
     return {
-      selectedTable: null,
-      selectedFreeze: null
+      selectedTable: '',
+      selectedFreeze: '',
+      selectedSex: '',
+      selectedAffectedStatus: ''
     }
   },
-  components: { FilterMenu, PageComponent, FilterFreeze },
+  components: { FilterMenu, PageComponent, FilterFreeze, FilterSex, FilterAffectedStatus },
   methods: {
     setContext (context) {
       console.log('context: ' + context)
@@ -62,5 +122,13 @@ export default {
     max-width: 25rem;
     min-width: 25rem;
     background-color: rgba(0,0,0,0.1);
+    form {
+      fieldset {
+        margin-bottom: 16px;
+      }
+      legend {
+        font-size: 12pt;
+      }
+    }
   }
 </style>
